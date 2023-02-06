fe56c31a037358e3e70e6ca6ace7e7d59cdf93d9
phyxius23 <30ntin@gmail.com>
2023-02-06 17:24:59 +0100

    compito

--- m1/s2/g1/compito/js/teamwork.js
+++ m1/s2/g1/compito/js/teamwork.js
@@ -4,12 +4,8 @@ if(isNaN(nome)) {
 
    document.getElementById('text').innerHTML = '<h1>Inserito con successo</h1>';
 
-   // document.write('Inserito con successo');
-
 } else {
 
    document.getElementById('text').innerHTML = '<h1>Inserire delle lettere i numeri non sono ammessi!</h1>';
-
-   // document.write("Inserire delle lettere i numeri non sono ammessi!");
 
 }
