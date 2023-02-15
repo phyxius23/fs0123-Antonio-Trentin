411ba2dea00cf0b24b592795fece6a00c6f79305
phyxius23 <30ntin@gmail.com>
2023-02-15 20:57:00 +0100

    ToDoList personale da ultimare

--- m1/s3/g3/compito/compito-personale/assets/js/scripts.js
+++ m1/s3/g3/compito/compito-personale/assets/js/scripts.js
@@ -1,5 +1,43 @@
 let input = document.getElementById('input');
-let btn = document.getElementById('btn');
+let btnInput = document.getElementById('btn-input');
+let listContent = document.getElementById('lists-content');
 
-console.log(btn.textContent);
+btnInput.addEventListener('click', createListItem); 
+
+function createListItem(){
+   let li = document.createElement('li');
+   let icon = document.createElement('i');
+   let btnDelete = document.createElement('button');
+
+   console.dir(li);
+
+   if(input.value == ''){
+      return
+   }
+   icon.classList.add('fal', 'fa-square');
+   li.append(icon);               // append button(delete) all'interno di li
+
+   // li.appendChild(input.value);
+   //textContent = input.value; // assegno al textContent il valore ricevuto in input
+   input.value = '';             // imposto il valore dell'elemento input a 'vuoto'
+
+   btnDelete.textContent = 'Cancella'; // scrivo all'interno del button(delete)
+
+   console.dir(icon);
+
+   listContent.appendChild(li);          // append li all'interno di ul
+   
+   btnDelete.addEventListener('click', ()=>{ // al click eseguo la linea di codice sotto
+      li.remove();                     // rimuovo l'elemento li
+   });
+   
+   // icon.classList.add('fal fa-square');
+   // input.value = '';
+
+   // let liContent = document.createTextNode(input.value)
+   // // li.appendChild(liContent);
+   // li.appendChild(icon);
+
+}
+
 
